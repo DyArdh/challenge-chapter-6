a6189464ea5dfd18d36e1afdf660901f2fb85c95
Andiko Yoga <90024747+DyArdh@users.noreply.github.com>
2023-05-19 18:00:38 +0700

    update config,js

--- config/config.js
+++ config/config.js
@@ -1,37 +1,36 @@
 require("dotenv").config();
-
 const {
+  DB_HOST,
+  DB_NAME,
   DB_USERNAME,
   DB_PASSWORD,
-  DB_NAME,
-  DB_HOST,
+  DB_DIALECT = "postgres",
   DB_PORT = 5432,
-  DB_DIALECT,
 } = process.env;
 
 module.exports = {
   development: {
+    host: DB_HOST,
+    database: DB_NAME,
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
     dialect: DB_DIALECT,
+    port: DB_PORT,
   },
   test: {
+    host: DB_HOST,
+    database: DB_NAME,
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
     dialect: DB_DIALECT,
+    port: DB_PORT,
   },
   production: {
+    host: DB_HOST,
+    database: DB_NAME,
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
     dialect: DB_DIALECT,
+    port: DB_PORT,
   },
 };
